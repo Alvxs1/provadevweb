dbb6b3ab50f8cd53e0980c58af0359ad7bd064e5
Eduardo Alves <151376400+Alvxs1@users.noreply.github.com>
2025-04-11 16:45:27 -0300

    Update exercício1.js

--- exercício1.js
+++ exercício1.js
@@ -1,1 +1,11 @@
-
+// Palavra reservada = function, usada para iniciar/criar a função
+// Nome da função = registrarPedido, é o nome dado a função, pode ser usado posteriormente no código para se referir a função
+//parâmetros cliente, prato,mesa,idade parâmetros são informações que uma função precisa para funcionar
+ function registrarPedido(cliente,prato,mesa,idade) {
+    return {
+      cliente:Paulo,
+      prato: Lasanha,
+      mesa: 1,
+      idade:18
+    };
+  }
